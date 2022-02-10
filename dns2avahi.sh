ec57248753ac927a7fb69b2cb66a6f71d4854965
Gonzalo Parra <gparra@adv-solution.net>
2022-02-10 10:38:14 -0500

    Update dns2avahi.sh

--- dns2avahi.sh
+++ dns2avahi.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-DOMAINS="domain.tdl [domain2.tdl]..." DNS_SERVER="dnsserver1 [dnsserver2]..." /usr/bin/python3 /home/pi/dns2avahi/avahi-publisher.py
+DOMAINS="domain.tld [domain2.tld]..." DNS_SERVER="dnsserver1 [dnsserver2]..." /usr/bin/python3 /home/pi/dns2avahi/avahi-publisher.py
